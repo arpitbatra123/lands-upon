387d519fa47cbcacbaf049d0a74baa642b2037b9
Arpit Batra <arpitbatra123@gmail.com>
2025-10-09 14:45:21 +0530

    add stats on homepage and fix feed generation

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,6 @@ module.exports = (eleventyConfig) => {
     eleventyConfig.addPassthroughCopy('favicon.svg');
     eleventyConfig.addPassthroughCopy('assets');
     eleventyConfig.addPassthroughCopy('script.js');
-    eleventyConfig.addPassthroughCopy('feed.xml');
     return {
         // Use liquid in html templates
         htmlTemplateEngine: 'liquid'
